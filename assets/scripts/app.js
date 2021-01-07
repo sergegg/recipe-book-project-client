33949b391e274fbdf0dadde45557343fdb8d5f89
sergey honiyan <sergehoniyan@gmail.com>
2021-01-06 19:02:36 -0500

    Added all auth functions
    Added all auth functions to HTML as well as added most form functions
    and required forms to HTML. Auth functions work, and the website is
    working through the grunt Serve.Edited config.js to include the
    herokuapp website.

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,5 +1,7 @@
 'use strict'
 
+const authEvents = require('./auth/events')
+const recipeEvents = require('./recipe/events')
 // use require with a reference to bundle the file and use it in this file
 // const example = require('./example')
 
@@ -8,4 +10,14 @@
 
 $(() => {
   // your JS code goes here
+  $('.auth-sign-in').hide()
+  $('.unconfirmed').show()
+  // set up the sign up
+  $('#sign-up').on('submit', authEvents.onSignUp)
+  // set up the sign up
+  $('#sign-in').on('submit', authEvents.onSignIn)
+  // set up the sign up
+  $('#change-pw').on('submit', authEvents.onChangePw)
+  // set up the sign up
+  $('#sign-out').on('click', authEvents.onSignOut)
 })
--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 let apiUrl
 const apiUrls = {
-  production: '<replace-with-heroku-url>',
+  production: 'https://sergeys-recipe-book-project.herokuapp.com/',
   development: 'http://localhost:4741'
 }
 
